88b067ec50f0997b0f06467ba1cbe6114545d8aa
Faizan Shabbir <faizanshabbir@users.noreply.github.com>
2015-03-03 08:12:20 -0800

    Import existing code
    Imported example code work done by Humza.

---
+++ lifelines_sample_2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Create lifetimes, but censor all lifetimes after time 12
+censor_after = 12
+actual_lifetimes = np.random.exponential(10, size=20)
+observed_lifetimes = np.minimum( actual_lifetimes, censor_after*np.ones(20) )
+C = (actual_lifetimes < censor_after) #boolean array
+
+from lifelines import KaplanMeierFitter
+
+kmf = KaplanMeierFitter()
+kmf.fit(observed_lifetimes, event_observed=C)
+
+# fitter methods have an internal plotting method.
+# plot the curve with the confidence intervals
+kmf.plot()
+
+import pylab
+pylab.show()
