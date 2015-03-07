8a1812aa6990fd67c0bd671ccdeafa82b9cbfd39
hbhakhra <bhakhrani.humza@gmail.com>
2015-03-06 19:04:09 -0800

    organize all non-site into non-site dir

--- non-site/python/lifelines_sample_2.py
+++ non-site/python/lifelines_sample_2.py
@@ -1,48 +1,38 @@
 import numpy as np
+from lifelines import KaplanMeierFitter
+
+def writeArrayToFile(varname,arrayToWrite):
+	filepath = 'log'+varname+'.txt'
+	with open(filepath,'w'):
+		pass
+		f = open(filepath, 'a+')
+		for data in arrayToWrite:
+			f.write(str(data)+'\n')
+		f.close()
 
 # Create lifetimes, but censor all lifetimes after time 12
 censor_after = 12
 actual_lifetimes = np.random.exponential(10, size=20)
-# print ("actual_lifetimes", actual_lifetimes)
-# print()
-observed_lifetimes = np.minimum( actual_lifetimes, censor_after*np.ones(20) )
-# print ("observed_lifetimes", observed_lifetimes)
+
+observed_lifetimes = np.minimum(actual_lifetimes, censor_after*np.ones(20) )
 C = (actual_lifetimes < censor_after) #boolean array
 
-from lifelines import KaplanMeierFitter
+## Write variables to files for debugging
+# varn = 'actual_lifetimes'
+# writeArrayToFile(varn,actual_lifetimes)
+# varn = 'observed_lifetimes'
+# writeArrayToFile(varn,observed_lifetimes)
+# varn = 'event_observed'
+# writeArrayToFile(varn,C)
+
 
 kmf = KaplanMeierFitter(1)
 kmf.fit(observed_lifetimes, event_observed=C)
+print (kmf)
 # fitter methods have an internal plotting method.
 # plot the curve with the confidence intervals
-# kmf.plot()
+#kmf.plot()
 
-# # Get data from kmf
-# l = dir(kmf)
-# d = kmf.__dict__
-
-from pprint import pprint
-# print("--dir--")
-# pprint(l)
-# print("--dict--")
-# pprint(d, indent=2)
-# print("--custom--")
-# timeline = getattr(kmf, "timeline")
-# print ("timeline")
-# pprint(timeline)
-# print("timeline_2", timeline[2])
-# confidence_interval = getattr(kmf, "confidence_interval_")
-# print ("confidence_interval", confidence_interval)
-# event_observed = getattr(kmf, "event_observed")
-# print ("event_observed", event_observed)
-# event_table = getattr(kmf, "event_table")
-# print ("event_table", event_table)
-survival_function_ = getattr(kmf, "survival_function_")
-estimate = getattr(survival_function_, "KM-estimate")
-print ("estimate", estimate)
-print ("--")
-for x in estimate:
-	print (x)
-# import pylab
-# pylab.show()
+import pylab
+pylab.show()
 # pylab.savefig('foo.svg', format="svg", bbox_inches='tight')
