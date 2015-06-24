6fc0e6fbfede3939f25855be5a04ed56225a3876
hbhakhra <bhakhrani.humza@gmail.com>
2015-06-24 00:54:59 -0700

    Turn off debugging and use postgres always

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,15 +92,20 @@ if hostname == "survive-this-curve":
         }
     }
 else:
-    DEBUG = True
-    TEMPLATE_DEBUG = True
-    
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+
     # Database
     # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'django',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'django',
+            'PASSWORD': '7WgZ1VnGQF',
+            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
         }
     }
 # Mailchimp API key
